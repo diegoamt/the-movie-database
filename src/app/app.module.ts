ed378eb547366ee9f91d9f68aab6d2865d5ffd57
Diego Andrés Monsalve Tellez <diegoamt.monsalve@gmail.com>
2017-08-15 10:47:45 -0500

    Agregado componente para el footer de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {RoutingModule} from './routing.module';
 
+import {ActorsService} from './actors/actors.service';
+import {MoviesService} from './movies/movies.service';
+import {ActorDetailService} from './actor-detail/actor-detail.service';
+import {MovieDetailService} from './movie-detail/movie-detail.service';
+
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {ActorsComponent} from './actors/actors.component';
 import {MoviesComponent} from './movies/movies.component';
 import {ActorDetailComponent} from './actor-detail/actor-detail.component';
 import {MovieDetailComponent} from './movie-detail/movie-detail.component';
-
-import {ActorsService} from './actors/actors.service';
-import {MoviesService} from './movies/movies.service';
-import {ActorDetailService} from './actor-detail/actor-detail.service';
-import {MovieDetailService} from './movie-detail/movie-detail.service';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import {MovieDetailService} from './movie-detail/movie-detail.service';
     ActorsComponent,
     MoviesComponent,
     ActorDetailComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
